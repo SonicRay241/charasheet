735b77110c97e449ab50ed733f710dbb8e73df8f
SonicRay241 <rayhan.permana@yahoo.com>
2026-08-29 11:16:22 +0700

    (main): Remove global navbar, as it is unused

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,16 +1,10 @@
-import { Outlet, createRootRoute, Link } from '@tanstack/react-router'
+import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { Toaster } from '@/components/ui/sonner'
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-3 flex gap-4 uppercase tracking-widest text-sm text-primary">
-        <Link to="/" className="px-1">
-          CHARASHEET
-        </Link>
-      </div>
-      <hr />
       <Outlet />
       <Toaster />
       <TanStackRouterDevtools position="bottom-right" />
--- src/routes/characters.$characterId/route.tsx
+++ src/routes/characters.$characterId/route.tsx
@@ -1,5 +1,6 @@
 import { Outlet, createFileRoute, Link } from "@tanstack/react-router";
 import { cn } from "@/lib/utils";
+import { ArrowLeftIcon } from "lucide-react";
 
 export const Route = createFileRoute("/characters/$characterId")({
   component: CharacterLayout
@@ -16,13 +17,25 @@ function CharacterLayout() {
   return (
     <div className="h-full">
       <div className="flex gap-4 px-3 pt-3">
+          <Link
+            to="/"
+            params={{ characterId }}
+            className={cn(
+              "terminal-label items-center gap-1 cursor-pointer flex px-1 pb-1",
+              "hover:bg-foreground hover:text-background",
+              "[&.active]:bg-primary [&.active]:text-primary-foreground",
+            )}
+          >
+            <ArrowLeftIcon size={12}/>
+            <span>Home</span>
+          </Link>
         {tabs.map((tab) => (
           <Link
             key={tab.to}
             to={tab.to}
             params={{ characterId }}
             className={cn(
-              "terminal-label cursor-pointer px-1 pb-1",
+              "terminal-label items-center gap-1 cursor-pointer px-1",
               "hover:bg-foreground hover:text-background",
               "[&.active]:bg-primary [&.active]:text-primary-foreground",
             )}
